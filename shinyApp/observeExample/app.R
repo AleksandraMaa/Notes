b4cc87954034e05e25e4423960e91a47a1312046
AleksandraMaa <tengma@berkeley.edu>
2020-09-11 18:59:04 -0700

    observeExample

---
+++ shinyApp/observeExample/app.R
@@ -0,0 +1,49 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+library(shiny)
+
+ui <- fluidPage(
+    selectInput(inputId = 'choice',
+                label = 'Choice',
+                choice = c('Hello','Goodbye'),
+                selected = c('Hello')
+    ),
+    
+    textOutput('result')
+    
+    
+)
+
+server <- function(input,output,session)({
+    
+    text <- reactiveValues()
+    
+    observe({
+        if (input$choice == 'Hello') {
+            text$result <- 'Hello World'
+        }
+    })
+    
+    observe({
+        if (input$choice == 'Goodbye') {
+            text$result <- 'Goodbye World'
+        }
+    })
+    
+    output$result <- renderText({
+        text$result
+    })
+    
+})
+
+
+shinyApp(ui = ui, server = server)
